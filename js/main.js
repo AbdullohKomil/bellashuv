5568380bc059e7846b05b0cb34a97acfc035ce25
AbdullohKomil <akomiljonov@icloud.com>
2023-03-11 20:48:05 +0500

    Edit and add

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 let LogoLink = document.querySelector('.logo');
+let LogoLink1 = document.querySelector('.logo1');
+
 
 const theme = () => {
   document.body.classList.toggle('dark');
   LogoLink.innerHTML = '';
+  LogoLink1.innerHTML = ''
   if (document.body.classList == 'dark') {
     const LogoImgDark = document.createElement('img');
     LogoImgDark.setAttribute('src', './images/logo-dark.svg');
     LogoLink.appendChild(LogoImgDark);
-  } else if (document.body.classList == '') {
+    LogoLink1.appendChild(LogoImgDark);
+  } else if (document.body.classList != 'dark') {
     const LogoImg = document.createElement('img');
     LogoImg.setAttribute('src', './images/logo.svg');
     LogoLink.appendChild(LogoImg);
+    LogoLink1.appendChild(LogoImg);
   }
 };
 
@@ -21,3 +26,11 @@ const menu = () => {
 const closeMenu = () => {
   document.body.classList.remove('menu-open');
 };
+
+
+
+const repleceBtn = document.querySelector('.projects__bottom__link');
+
+repleceBtn.addEventListener('click', () => {
+  location.replace('portfolio.html')
+})
